48d3cf79b218ef9a6f468cacdfe3892da1b0eb04
v-aguiar <v.aguiar.dev@gmail.com>
2022-04-28 00:01:02 -0300

    Add GET '/participants' route, which answers with a list containing all participants

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 ﻿import express, { json } from "express";
 import cors from "cors";
 import chalk from "chalk";
+import dotenv from "dotenv";
+
+import { MongoClient } from "mongodb";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db = null;
+
+// GET all participants list
+app.get("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db("batepapo-uol-api");
+
+    const participants = await db.collection("participants").find({}).toArray();
+    res.status(200).send(participants);
+    mongoClient.close();
+  } catch (e) {
+    console.error(e);
+    res.send("400");
+    mongoClient.close();
+  }
+});
+
 app.listen(5000, () =>
   console.log(
     chalk.bold.greenBright("\n🚀 Server is runnin!") +
-      chalk.bold.cyanBright("\n\nLlistening on port 5000...")
+      chalk.bold.cyanBright("\n\nListening on port 5000...")
   )
 );
